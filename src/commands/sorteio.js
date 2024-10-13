8f6ace867825cafefea1d41c012c6cfcdf6519b3
luqas31 <lucas.ers@icloud.com>
2024-10-13 20:24:38 +0100

    fixed bugs

--- src/commands/sorteio.js
+++ src/commands/sorteio.js
@@ -7,42 +7,60 @@ export const execute = async interaction => {
 	try {
 		await interaction.deferReply();
 
-		await interaction.editReply('Quantos jogadores participarão?');
+		const question1 = await interaction.editReply('Quantos jogadores participarão?');
 		const filter = m => m.author.id === interaction.user.id;
 
 		const playerCountMessage = await getMessageWithTimeout(interaction, filter);
 		if (!playerCountMessage) {
+			await question1.delete();
 			return await interaction.editReply('Tempo esgotado. O sorteio foi cancelado.');
 		}
 
 		const playerCount = parseInt(playerCountMessage.content);
 		if (isNaN(playerCount)) {
+			await question1.delete();
+			await playerCountMessage.delete();
 			return await interaction.editReply('Por favor, insira um número válido de jogadores.');
 		}
 
-		await interaction.followUp(`Por favor, digite os nomes dos ${playerCount} jogadores, separados por vírgula:`);
+		await question1.delete();
+		await playerCountMessage.delete();
+
+		const question2 = await interaction.followUp(`Por favor, digite os nomes dos ${playerCount} jogadores, separados por vírgula:`);
 
 		const playersMessage = await getMessageWithTimeout(interaction, filter);
 		if (!playersMessage) {
+			await question2.delete();
 			return await interaction.followUp('Tempo esgotado. O sorteio foi cancelado.');
 		}
 
 		const players = playersMessage.content.split(',').map(player => player.trim());
 		if (players.length !== playerCount) {
+			await question2.delete();
+			await playersMessage.delete();
 			return await interaction.followUp('O número de jogadores fornecido não corresponde ao número de nomes inseridos.');
 		}
 
-		await interaction.followUp('Agora, liste os tipos de armas, separados por vírgula (ex.: arma1, arma2, arma3...):');
+		await question2.delete();
+		await playersMessage.delete();
+
+		const question3 = await interaction.followUp('Agora, liste os tipos de armas, separados por vírgula (ex.: arma1, arma2, arma3...):');
 
 		const weaponsMessage = await getMessageWithTimeout(interaction, filter);
 		if (!weaponsMessage) {
+			await question3.delete();
 			return await interaction.followUp('Tempo esgotado. O sorteio foi cancelado.');
 		}
 
 		const weapons = weaponsMessage.content.split(',').map(weapon => weapon.trim());
 		if (weapons.length < players.length) {
+			await question3.delete();
+			await weaponsMessage.delete();
 			return interaction.followUp('Número insuficiente de armas! O número de armas deve ser igual ou maior que o número de jogadores.');
 		}
+
+		await question3.delete();
+		await weaponsMessage.delete();
 
 		players.sort(() => Math.random() - 0.5);
 		weapons.sort(() => Math.random() - 0.5);
@@ -58,24 +76,8 @@ export const execute = async interaction => {
 			playerWeaponMap.get(player).push(weapon);
 		});
 
-		const finalMessages = [];
 		for (const [player, weapons] of playerWeaponMap.entries()) {
-			const message = await interaction.channel.send(`${player} - ${weapons.join(', ')}`);
-			finalMessages.push(message);
-		}
-
-		try {
-			const messagesToDelete = [];
-			const fetchedMessages = await interaction.channel.messages.fetch({ limit: 100 });
-			fetchedMessages.forEach(message => {
-				if ((message.author.id === interaction.user.id || message.author.bot) && !finalMessages.some(finalMessage => finalMessage.id === message.id)) {
-					messagesToDelete.push(message);
-				}
-			});
-
-			await interaction.channel.bulkDelete(messagesToDelete);
-		} catch (error) {
-			console.error('Erro ao apagar mensagens:', error);
+			await interaction.channel.send(`${player} - ${weapons.join(', ')}`);
 		}
 	} catch (error) {
 		console.error('Erro ao executar o sorteio:', error);
